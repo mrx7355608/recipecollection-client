32478cebcbd212f7c5a7fb14a7d0f7576f028abe
mrx7355608 <mrx7355608@gmail.com>
2023-06-12 00:23:37 +0500

    created a recipe page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Recipe from './pages/Recipe';
 import { useEffect } from 'react';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home />,
+    },
+    {
+        path: '/recipes/:id',
+        element: <Recipe />,
     },
     {
         path: '/auth',
---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,43 @@
+import { Flex, Box } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import CommentSection from '../components/Recipe/CommentSection';
+import Ingredients from '../components/Recipe/Ingredients';
+import Steps from '../components/Recipe/Steps';
+import Details from '../components/Recipe/Details';
+import ChefInfo from '../components/Recipe/ChefInfo';
+
+export default function Recipe() {
+    const ingredients = [
+        'Potato 2Kgs',
+        'Green chilly 2 bowls',
+        'Garlic 30 grams',
+        'Ketchup 30ml',
+    ];
+    const steps = [
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+        'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+    ];
+
+    return (
+        <>
+            <Navbar />
+            <Flex direction={'column'} p="6">
+                {/* DETAILS */}
+                <Details />
+
+                <Flex gap="3" h="full">
+                    <Box w="full">
+                        <Ingredients ingredients={ingredients} />
+                        <Steps steps={steps} />
+                    </Box>
+                    <ChefInfo />
+                </Flex>
+                <CommentSection />
+            </Flex>
+        </>
+    );
+}
