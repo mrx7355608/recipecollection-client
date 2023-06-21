4eaf9335f845b7c40638dbd61518f305a21d2968
mrx7355608 <mrx7355608@gmail.com>
2023-06-21 11:09:37 +0500

    changed styling

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,21 +1,15 @@
 import { HStack, Box } from '@chakra-ui/react';
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function AuthLayout() {
-    useEffect(() => {
-        document.body.style.background = 'white !important';
-    }, []);
-
     return (
-        <HStack gap="3" h="100vh">
+        <HStack h="100vh">
             <Box
                 backgroundImage="/bg.jpeg"
                 backgroundSize="cover"
                 backgroundPosition="center"
-                w="47%"
+                w="50%"
                 h="100%"
-                style={{}}
             ></Box>
             <Outlet />
         </HStack>
