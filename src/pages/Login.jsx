ac365ee893cc9227174e16f0f5deb9959d3afe59
mrx7355608 <mrx7355608@gmail.com>
2023-06-21 11:10:57 +0500

    added login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,64 +1,98 @@
-import React from 'react';
+import { useState } from 'react';
 import {
     Text,
-    Container,
+    Box,
     Heading,
     Input,
     Button,
     VStack,
     Center,
 } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../services/auth';
+import { useUser } from '../contexts/user';
 
 export default function Login() {
+    const [error, setError] = useState(null);
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: '',
+    });
+    const { userDispatcher } = useUser();
+    const navigateTo = useNavigate();
+
     return (
-        <VStack justifyContent="center" h="100%">
-            <Container
+        <VStack
+            alignItems={'flex-start'}
+            justifyContent="center"
+            h="100%"
+            bg="white"
+            w="50%"
+            m="0"
+        >
+            <Box
                 p="6"
+                pl="7"
                 borderRadius="lg"
                 justifyContent="center"
                 bg="white"
-                w="max-content"
+                w="80%"
             >
-                <Heading mb="10" fontSize="3xl" color="gray.700">
+                <Heading mb="5" fontSize="3xl" color="gray.700">
                     Login to your account
                 </Heading>
-                <Text mt="6" mb="4"></Text>
-                <Input
-                    borderWidth="2px"
-                    fontSize="sm"
-                    borderColor="gray.600"
-                    size="lg"
-                    borderRadius="none"
-                    placeholder="Email address"
-                    type="email"
-                    name="email"
-                    _placeholder={{ color: 'gray.800' }}
-                />
-                <Input
-                    borderWidth="2px"
-                    fontSize="sm"
-                    borderColor="gray.600"
-                    size="lg"
-                    borderRadius="none"
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    mt="4"
-                    _placeholder={{ color: 'gray.800' }}
-                />
-                <Button
-                    size="lg"
-                    borderRadius="none"
-                    colorScheme="yellow"
-                    w="full"
-                    mt="9"
-                    mb="7"
-                    fontSize="sm"
-                    variant="solid"
-                >
-                    LOGIN
-                </Button>
+                {error ? (
+                    <Text
+                        my="6"
+                        p="3"
+                        color="red.700"
+                        borderRadius={'sm'}
+                        w="full"
+                        bg="red.100"
+                    >
+                        {error}
+                    </Text>
+                ) : null}
+                <form onSubmit={loginUser}>
+                    <Input
+                        borderWidth="2px"
+                        fontSize="sm"
+                        borderColor="gray.600"
+                        size="lg"
+                        borderRadius="none"
+                        placeholder="Email address"
+                        type="email"
+                        name="email"
+                        _placeholder={{ color: 'gray.800' }}
+                        onChange={handleOnChange}
+                    />
+                    <Input
+                        borderWidth="2px"
+                        fontSize="sm"
+                        borderColor="gray.600"
+                        size="lg"
+                        borderRadius="none"
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        mt="4"
+                        _placeholder={{ color: 'gray.800' }}
+                        onChange={handleOnChange}
+                    />
+                    <Button
+                        size="lg"
+                        borderRadius="none"
+                        colorScheme="yellow"
+                        w="full"
+                        mt="9"
+                        mb="7"
+                        fontSize="sm"
+                        variant="solid"
+                        type="submit"
+                    >
+                        LOGIN
+                    </Button>
+                </form>
                 <Center>
                     Don&apos;t have an account?
                     <Text
@@ -70,7 +104,28 @@ export default function Login() {
                         <Link to="/auth/signup">Signup</Link>
                     </Text>
                 </Center>
-            </Container>
+            </Box>
         </VStack>
     );
+
+    function handleOnChange(e) {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+    }
+    function handleError(errorMessage) {
+        setError(errorMessage);
+        return setTimeout(() => setError(null), 5000);
+    }
+
+    async function loginUser(e) {
+        e.preventDefault();
+        const { error, data } = await login(loginData);
+        if (error) return handleError(error);
+        // Update user state
+        userDispatcher({ type: 'LOGIN', user: data.user });
+        // Navigate user back to homapage
+        // TODO: change the navigation from homepage, to the page user came from
+        // like a redirect back
+        navigateTo('/');
+    }
 }
