4ccc062f0ba902a7d9e0ec8ddf2053f8b03e1a2d
mrx7355608 <mrx7355608@gmail.com>
2023-06-21 11:10:05 +0500

    wrapped whole app with user context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { UserProvider } from './contexts/user.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            <UserProvider>
+                <App />
+            </UserProvider>
         </ChakraProvider>
     </React.StrictMode>
 );
