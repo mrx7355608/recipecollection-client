6a21761d2552a60edbc573d5859fa136342b0436
mrx7355608 <mrx7355608@gmail.com>
2023-06-24 15:17:11 +0500

    moved getUserData func from main layout to app.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,26 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { useEffect } from 'react';
 import Footer from '../components/Footer';
-import { getUserData } from '../services/user';
-import { useUser } from '../contexts/user';
 
 export default function MainLayout() {
-    const { userDispatcher } = useUser();
-    useEffect(() => {
-        document.body.style.background = 'rgb(245, 245, 245)';
-
-        // Fetch user data
-        getUserData()
-            .then((resp) => {
-                userDispatcher({
-                    type: 'FETCHED_USER_DATA',
-                    user: resp.data.user,
-                });
-            })
-            .catch(console.log);
-    }, []);
-
     return (
         <>
             <Navbar />
