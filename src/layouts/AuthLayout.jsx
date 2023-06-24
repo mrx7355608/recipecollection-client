dcb9332b5f37792eae0e4e0b7ff00b9d3c89df11
mrx7355608 <mrx7355608@gmail.com>
2023-06-24 15:13:34 +0500

    protecting guest routes

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,17 @@
 import { HStack, Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../contexts/user';
 
 export default function AuthLayout() {
+    const { user } = useUser();
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+        if (user) return navigateTo('/');
+    }, [user]);
+
     return (
         <HStack h="100vh">
             <Box
