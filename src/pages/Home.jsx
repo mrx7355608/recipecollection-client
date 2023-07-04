361c446a5e943f20c4dc3a290d9d99e548b22aad
mrx7355608 <mrx7355608@gmail.com>
2023-07-04 09:49:59 +0500

    populating homepage with api data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import { Flex, Heading, Box, Button } from '@chakra-ui/react';
-import Navbar from '../components/Navbar';
+import { Flex, Heading, Box, Spinner } from '@chakra-ui/react';
 import Hero from '../components/Hero';
 import RecipesList from '../components/RecipesList';
 import Searchbar from '../components/Searchbar';
+import useRecipes from '../hooks/useRecipes';
 
 export default function Home() {
-    const countries = ['Pakistani', 'Indian', 'French', 'Italian', 'German'];
+    const { recipes, error, loading } = useRecipes();
 
     return (
         <>
@@ -15,30 +15,19 @@ export default function Home() {
                 <Heading color="gray.700" mt="12" ml="5" mb="5">
                     Find your recipe
                 </Heading>
-                <Flex
-                    alignItems={'center'}
-                    justifyContent={'space-evenly'}
-                    gap="4"
-                >
-                    <Searchbar />
-                    {countries.map((item, index) => {
-                        return (
-                            <Button
-                                key={index}
-                                p="1"
-                                variant="outline"
-                                fontWeight={'semibold'}
-                                px="3"
-                                borderRadius={'sm'}
-                                borderWidth={2}
-                                borderColor={'gray.600'}
-                            >
-                                {item}
-                            </Button>
-                        );
-                    })}
-                </Flex>
-                <RecipesList />
+                <Searchbar />
+                {loading ? (
+                    <Flex
+                        w="100vw"
+                        my="12"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Spinner color="yellow.500" />
+                    </Flex>
+                ) : null}
+                {error ? <Heading>{error}</Heading> : null}
+                <RecipesList recipes={recipes} />
             </Flex>
         </>
     );
