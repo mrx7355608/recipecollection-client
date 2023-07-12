d92a8ed4b3f066cf2ea4bc439c46774c56c12117
mrx7355608 <mrx7355608@gmail.com>
2023-07-12 10:40:32 +0500

    added form to search on enter keypress

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,33 +1,49 @@
 import { Flex, Button, Input } from '@chakra-ui/react';
 import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Searchbar() {
+    const [query, setQuery] = useState('');
+    const navigateTo = useNavigate();
+
     return (
-        <Flex
-            gap="1"
-            alignItems="center"
-            ml="5"
-            w="md"
-            bg="white"
-            borderRadius="sm"
-            borderWidth={2}
-            borderColor={'gray.600'}
-        >
-            <Input
-                bg="transparent"
-                size="lg"
-                placeholder="Search"
-                variant="ghost"
-                h="min-content"
-            />
-            <Button
-                bg="transparent"
-                size="md"
-                variant="solid"
-                borderRadius="sm"
+        <Flex>
+            <form
+                style={{
+                    display: 'flex',
+                    gap: '1',
+                    alignItems: 'center',
+                    marginLeft: '1rem',
+                    width: '500px',
+                    background: 'white',
+                    borderRadius: '2px',
+                    borderWidth: 2,
+                    borderColor: '#1e1e1e',
+                }}
+                onSubmit={() => navigateTo(`/search?query=${query}`)}
             >
-                <FaSearch />
-            </Button>
+                <Input
+                    bg="transparent"
+                    size="lg"
+                    placeholder="Search"
+                    variant="ghost"
+                    h="min-content"
+                    onChange={(e) => {
+                        const { value } = e.target;
+                        setQuery(value);
+                    }}
+                />
+                <Button
+                    bg="transparent"
+                    size="md"
+                    variant="solid"
+                    borderRadius="sm"
+                    type="submit"
+                >
+                    <FaSearch />
+                </Button>
+            </form>
         </Flex>
     );
 }
